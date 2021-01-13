e171ed7ce0d7506542f3e1ad3891ebe541cf5775
Dan Hill <dhill@promoted.ai>
2021-01-12 19:30:30 -0800

    feat: add hoc
    TESTING=unit test

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { useImpressionTracker } from '.';
+import { WithImpressionTrackerProps, useImpressionTracker, withImpressionTracker } from '.';
 import 'intersection-observer';
 
 interface Props {
@@ -8,7 +8,7 @@ interface Props {
 }
 
 // A simple component for testing useImpressionTracker.
-export const ExampleComponent = ({ text }: Props) => {
+export const HookedExampleComponent = ({ text }: Props) => {
   const [ref, impressionId, logImpression] = useImpressionTracker({
     insertionId: 'abc',
     logImpression: (impressionId) => console.error(impressionId),
@@ -27,9 +27,40 @@ export const ExampleComponent = ({ text }: Props) => {
 
 describe('useImpressionTracker', () => {
   it('just make sure simple render works', () => {
-    const { getByText } = render(<ExampleComponent text="component works" />);
+    const { getByText } = render(<HookedExampleComponent text="component works" />);
     expect(getByText('component works')).toBeInTheDocument();
   });
+  // TODO - add tests for interactions.
+});
 
+interface WrappedProps extends WithImpressionTrackerProps {
+  text: string;
+}
+
+// A simple component for testing useImpressionTracker.
+export const WrappedExampleComponent = ({ impressionId, impressionRef, logImpression, text }: WrappedProps) => {
+  if (impressionId === null) {
+    throw Error('impressionId should not be null');
+  }
+  if (logImpression === null) {
+    throw Error('logImpression should not be null');
+  }
+  return <div ref={impressionRef}>{text}</div>;
+};
+
+const TrackedExampleComponent = withImpressionTracker(
+  WrappedExampleComponent,
+  () => 'test-insertion',
+  () => null,
+  (err) => {
+    throw err;
+  }
+);
+
+describe('useImpressionTracker', () => {
+  it('just make sure simple render works', () => {
+    const { getByText } = render(<TrackedExampleComponent text="component works" />);
+    expect(getByText('component works')).toBeInTheDocument();
+  });
   // TODO - add tests for interactions.
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from 'react';
+import { Subtract } from 'utility-types';
+import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import 'intersection-observer';
 import { useInView } from 'react-intersection-observer';
 // TODO - switch to dynamic import approach.
+
+// TODO - support polyfill.
+// https://www.npmjs.com/package/react-intersection-observer#polyfill
 
 // Simple logic for now.  A piece of content must have been continuously viewed
 // for the visibilityTimeThreshold.
@@ -10,12 +14,19 @@ const DEFAULT_VISIBILITY_RATIO_THRESHOLD = 0.5;
 const DEFAULT_VISIBILITY_TIME_THRESHOLD = 1000;
 
 interface TrackerProps {
+  /* The (pre-impression) insertionId to log on the impressionId. */
   insertionId: string;
+  /* Called when we should log an impression. */
   logImpression: (impressionId: string) => void;
+  /* Called when an error occurs. */
   handleLogError: (err: Error) => void;
+  /* To override the visibility threshold. */
   visibleRatioThreshold?: number;
+  /* To override the visibility threshold. */
   visibilityTimeThreshold?: number;
 }
+
+type TrackerResponse = [(node?: Element | null) => void, string, () => void];
 
 /*
 Calls `logImpression` if a insertion `ref` is viewed log enough to be considered
@@ -32,7 +43,7 @@ ready in case an item is clicked on without fully qualifying for an impression.
 Uses the `react-intersection-observer` to keep track of visibility changes.
 This won't work with older browsers.
 */
-export const useImpressionTracker = (props: TrackerProps) => {
+export const useImpressionTracker = (props: TrackerProps): TrackerResponse => {
   const {
     insertionId,
     logImpression,
@@ -40,63 +51,99 @@ export const useImpressionTracker = (props: TrackerProps) => {
     visibleRatioThreshold = DEFAULT_VISIBILITY_RATIO_THRESHOLD,
     visibilityTimeThreshold = DEFAULT_VISIBILITY_TIME_THRESHOLD,
   } = props;
-  try {
-    const [ref, inView] = useInView({
-      threshold: visibleRatioThreshold,
-    });
-    const [prevInsertionId, setInsertionId] = useState('');
-    const [impressionId, setImpressionId] = useState('');
-    const [logged, setLogged] = useState(false);
+  if (typeof window !== 'undefined' && typeof window.IntersectionObserver !== 'undefined') {
+    try {
+      const [ref, inView] = useInView({
+        threshold: visibleRatioThreshold,
+      });
+      const [prevInsertionId, setInsertionId] = useState('');
+      const [impressionId, setImpressionId] = useState('');
+      const [logged, setLogged] = useState(false);
 
-    // Generate a new UUID on mount.
-    useEffect(() => {
-      // This React hook is designed to be used with only one Insertion.
-      if (prevInsertionId !== '' && insertionId !== prevInsertionId) {
-        handleLogError(new Error('The same useImpressionTracker should not be used with multiple insertions'));
-      }
-      setInsertionId(insertionId);
-      // Only generate an impressionId if not set.
-      if (impressionId === '') {
-        setImpressionId(uuidv4());
-      }
-    }, [insertionId]);
-
-    const _logImpression = () => {
-      if (!logged) {
-        setLogged(true);
-        // In case there is a weird corner case where impressionId has not been set.
+      // Generate a new UUID on mount.
+      useEffect(() => {
+        // This React hook is designed to be used with only one Insertion.
+        if (prevInsertionId !== '' && insertionId !== prevInsertionId) {
+          handleLogError(new Error('The same useImpressionTracker should not be used with multiple insertions'));
+        }
+        setInsertionId(insertionId);
+        // Only generate an impressionId if not set.
         if (impressionId === '') {
           setImpressionId(uuidv4());
         }
-        logImpression(impressionId);
-      }
-    };
+      }, [insertionId]);
 
-    useEffect(
-      () => {
-        if (!inView || logged) {
-          return;
+      const _logImpression = () => {
+        if (!logged) {
+          setLogged(true);
+          // In case there is a weird corner case where impressionId has not been set.
+          if (impressionId === '') {
+            setImpressionId(uuidv4());
+          }
+          logImpression(impressionId);
         }
-        const timer = setTimeout(_logImpression, visibilityTimeThreshold);
-        return () => clearTimeout(timer);
-      },
-      // TODO - should ref.current be in this?
-      // The Typescript interface for the useInView hook is limited.
-      // @ts-expect-error ref.current is not in the type.
-      [ref.current, inView]
-    );
+      };
 
-    return [ref, impressionId, _logImpression];
-  } catch (error) {
-    handleLogError(error);
-    return [
-      () => {
-        /* do nothing */
-      },
-      '',
-      () => {
-        /* do nothing */
-      },
-    ];
+      useEffect(
+        () => {
+          if (!inView || logged) {
+            return;
+          }
+          const timer = setTimeout(_logImpression, visibilityTimeThreshold);
+          return () => clearTimeout(timer);
+        },
+        // TODO - should ref.current be in this?
+        // The Typescript interface for the useInView hook is limited.
+        // @ts-expect-error ref.current is not in the type.
+        [ref.current, inView]
+      );
+
+      return [ref, impressionId, _logImpression];
+    } catch (error) {
+      handleLogError(error);
+    }
   }
+  return [
+    () => {
+      /* do nothing */
+    },
+    '',
+    () => {
+      /* do nothing */
+    },
+  ];
+};
+
+export interface WithImpressionTrackerProps {
+  impressionRef: (node?: Element | null) => void;
+  impressionId: string;
+  logImpression: () => void;
+}
+
+/**
+ * An HOC version of useImpressionProps.  If you can, use the hook instead.
+ * @param Component          the Component to wrap
+ * @param getInsertionId     gets the insertionId from the props
+ * @param innerLogImpression your logging code
+ * @param handleLogError     what to do with errors
+ */
+export const withImpressionTracker = <P extends WithImpressionTrackerProps>(
+  Component: React.ComponentType<P>,
+  getInsertionId: (props: Subtract<P, WithImpressionTrackerProps>) => string,
+  innerLogImpression: (impressionId: string) => void,
+  handleLogError: (err: Error) => void
+): React.FC<Subtract<P, WithImpressionTrackerProps>> => (props: Subtract<P, WithImpressionTrackerProps>) => {
+  const [impressionRef, impressionId, logImpression] = useImpressionTracker({
+    insertionId: getInsertionId(props),
+    logImpression: innerLogImpression,
+    handleLogError,
+  });
+  return (
+    <Component
+      {...(props as P)}
+      impressionRef={impressionRef}
+      impressionId={impressionId}
+      logImpression={logImpression}
+    />
+  );
 };
